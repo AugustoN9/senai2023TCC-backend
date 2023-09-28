c38fd158fd79735f398490db0be8015e5f56a8ac
Augusto C C López <41811982+AugustoN9@users.noreply.github.com>
2023-09-28 00:57:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const exame = require("./src/router/exame.router");
 const app = express();
 const port = 3007;
 
-// app.use(express.json());
+app.use(express.json());
 // app.use(cors(
 //     {
 //         origin: "https://senai2023-tcc-frontend.vercel.app/",
@@ -24,15 +24,15 @@ const port = 3007;
 //     }
 // ));
 
-app.use((req, res, next) => {
+app.use(cors());
 
-    res.header("Access-Control-Allow-Origin", "https://senai2023-tcc-frontend.vercel.app/");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    app.use(cors());
-    next();
-
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://senai2023-tcc-frontend.vercel.app/");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type");
+//     app.use(cors());
+//     next();
+// });
 
 
 app.post("/login", async (req, res ) => {
