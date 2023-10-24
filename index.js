5cb694d7c46e9227ba307a5ddc1e0cdb4e6a270e
Augusto C C López <41811982+AugustoN9@users.noreply.github.com>
2023-10-23 23:39:58 -0300

    Update index.js resolvido problema de CORS

--- index.js
+++ index.js
@@ -17,15 +17,6 @@ const app = express();
 const port = 3007;
 
 
-// app.use(cors(
-//     {
-//         optionsSuccessStatus: 200, // For legacy browser support
-//         credentials: true, // This is important.
-//         origin: "https://exactapatho.netlify.app",
-//         methods:["GET", "POST", "PUT", "PATCH", "DELETE"]
-//     }
-// ));
-
 app.use(cors(
     {
         origin: "*",
@@ -34,45 +25,7 @@ app.use(cors(
 ))
 app.options('*', cors())
 
-// app.use(cors(
-//     {
-//             "origin": "*",
-//             "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//             "preflightContinue": false,
-//             "optionsSuccessStatus": 204
-//     }
-// ));
-
 app.use(express.json());
-
-
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", 'GET, POST, PUT, PATCH, DELETE');
-//     app.use(cors());
-//     next();
-// });
-
-// const corsOptions = {
-//     optionsSuccessStatus: 200, // For legacy browser support
-//     credentials: true, // This is important.
-//     origin: "https://senai2023-tcc-frontend.vercel.app/",
-// };
-
-// app.use(cors(corsOptions));
-
-
-
-
-
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://senai2023-tcc-frontend.vercel.app/");
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type");
-//     app.use(cors());
-//     next();
-// });
-
 
 app.post("/login", async (req, res ) => {
     try{
